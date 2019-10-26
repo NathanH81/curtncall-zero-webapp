e4666b2be050cfc1da4fedb0e084d2e15c7c8a06
Nathan Hall <HannahTall@yahoo.com>
2019-10-25 19:14:55 -0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,46 @@
-import React from "react";
-import Navbar from "./common/navbar/Navbar";
+import React, { useState, useCallback } from "react";
 import { useStore } from "./store";
+import { animated, useTransition } from "react-spring";
+
+const pages = [
+  (props: { style: any }) => (
+    <animated.div className="curtain" style={{ ...props.style }}>
+      CurtnCall
+    </animated.div>
+  ),
+  (props: { style: any }) => (
+    <animated.div className="background" style={{ ...props.style }}>
+      CurtnCall
+    </animated.div>
+  )
+];
 
 const App: React.FC = () => {
+  const [index, set] = useState(0);
+  const onClick = useCallback(() => set(state => (state === 0 ? 1 : 0)), []);
+
+  const transitions = useTransition(index, p => p, {
+    from: { opacity: 0, transform: "translate3d(0,100%,0)" },
+    enter: { opacity: 1, transform: "translate3d(0,0,0)" },
+    leave: { opacity: 0, transform: "translate3d(0,100%,0)" }
+  });
+
   return (
-    <div>
-      <Navbar />
+    <div className="background-wrapper" onClick={onClick}>
+      {transitions.map(({ item, props, key }) => {
+        const Page = pages[item];
+        return <Page key={key} style={props} />;
+      })}
     </div>
   );
+
+  // return (
+  //   <div className="background-wrapper">
+
+  //     <animated.div className="curtain" style={props}></animated.div>
+  //     {/* <div className="background"></div> */}
+  //   </div>
+  // );
 };
 
 export default App;
--- src/common/navbar/Navbar.tsx
+++ src/common/navbar/Navbar.tsx
@@ -1,41 +1,41 @@
 import React from "react";
-import styled from "styled-components";
 
-const Logo = styled.div`
-  color: white;
-  text-transform: uppercase;
-  letter-spacing: 0.1rem;
-`;
+// const Logo = styled.div`
+//   color: white;
+//   text-transform: uppercase;
+//   letter-spacing: 0.1rem;
+// `;
 
-const Nav = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  min-height: 8vh;
-  background-color: #5d4954;
-`;
+// const Nav = styled.div`
+//   display: flex;
+//   justify-content: space-around;
+//   align-items: center;
+//   min-height: 8vh;
+//   background-color: #5d4954;
+// `;
 
 const Navbar: React.FC = () => {
   return (
-    <Nav>
-      <Logo>
-        <h4>The Nav</h4>
-      </Logo>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">About</a>
-        </li>
-        <li>
-          <a href="/">Work</a>
-        </li>
-        <li>
-          <a href="/">Projects</a>
-        </li>
-      </ul>
-    </Nav>
+    <div>Test</div>
+    // <Nav>
+    //   <Logo>
+    //     <h4>The Nav</h4>
+    //   </Logo>
+    //   <ul>
+    //     <li>
+    //       <a href="/">Home</a>
+    //     </li>
+    //     <li>
+    //       <a href="/">About</a>
+    //     </li>
+    //     <li>
+    //       <a href="/">Work</a>
+    //     </li>
+    //     <li>
+    //       <a href="/">Projects</a>
+    //     </li>
+    //   </ul>
+    // </Nav>
   );
 };
 
